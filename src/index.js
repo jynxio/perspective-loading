e78ce41ee51857af38f293b50643e00e21f78ff0
jynxio <jinxiaomatrix@gamil.com>
2023-08-27 18:33:52 +0800

    u

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import Stats from 'stats.js';
 import earcut from 'earcut';
 import * as three from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { OutlineEffect } from 'three/examples/jsm/effects/OutlineEffect';
+import { Line2 } from 'three/examples/jsm/lines/Line2';
+import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
+import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry';
 
 /* Camera */
 const camera = new three.PerspectiveCamera(75, globalThis.innerWidth / globalThis.innerHeight, 0.01, 100);
@@ -20,6 +22,10 @@ const canvas = document.createElement('canvas');
 const renderer = new three.WebGLRenderer({ canvas, antialias: globalThis.devicePixelRatio < 2 });
 
 document.body.prepend(canvas);
+renderer.autoClear = false;
+renderer.autoClearColor = false;
+renderer.autoClearDepth = false;
+renderer.autoClearStencil = false;
 renderer.setPixelRatio(Math.min(globalThis.devicePixelRatio, 2));
 renderer.setSize(globalThis.innerWidth, globalThis.innerHeight);
 
@@ -43,12 +49,20 @@ globalThis.addEventListener('resize', _ => {
 });
 
 /* Render */
-const effect = new OutlineEffect(renderer);
 renderer.setAnimationLoop(function loop() {
     stats.begin();
 
+    renderer.clearColor();
+    renderer.clearStencil();
+
     controls.update();
-    effect.render(scene, camera);
+    scene.remove(fatLine).add(loading);
+    renderer.render(scene, camera);
+
+    renderer.clearDepth();
+
+    scene.remove(loading).add(fatLine);
+    renderer.render(scene, camera);
 
     stats.end();
 });
@@ -87,52 +101,82 @@ requestAnimationFrame(function loop() {
 });
 
 /*  */
-// const segmentCount = 13;
-// const directions = [];
+const segmentCount = 13;
+const directions = []; // 0 -> 0, 1 -> 90, -1 -> -90
 
-// for (let i = 0; i < segmentCount; i++) {
-//     const rad = ((i * Math.PI) / 2) % (Math.PI * 2);
-//     const sin = Math.sin(rad);
-//     directions.push(Math.abs(sin) < Number.EPSILON ? 0 : sin);
-// }
+for (let i = 0; i < segmentCount; i++) {
+    const rad = ((i * Math.PI) / 2) % (Math.PI * 2);
+    const sin = Math.sin(rad);
 
-// // const temp = [-5, 0, -3, 0, -3, 2, -2, 2, -2, -2, -1, -2, -1, 3, 0, 3, 0, -3, 2, -3, 2, 2, 3, 2, 3, 0, 4, 0];
-// const temp = [-5, 0, -3, 0, -3, 2];
-// const positionArray = [];
+    directions.push(Math.abs(sin) < Number.EPSILON ? 0 : sin);
+}
 
-// for (let i = 0; i < temp.length; i += 2) {
-//     const x = temp[i + 0];
-//     const z = temp[i + 1];
+//
+const geometries = [];
 
-//     positionArray.push(x, 0.2, z);
-// }
+for (let i = 0; i < segmentCount; i++) {
+    geometries.push(new three.PlaneGeometry(1, 1)); // TODO: 使用InstanceGeometry
+}
 
-// for (let i = temp.length - 1; i >= 0; i -= 2) {
-//     const x = temp[i - 1];
-//     const z = temp[i - 0];
-//     positionArray.push(x, -0.2, z);
-// }
-
-// console.log(positionArray); // earcut出错了，它不能剖一个异面多边形，你要把异面多边形拆开来给earcut处理 // TODO:
-
-// const positionArray = [-1, 1, 0, 1, 1, 0, 1, -1, 0, -1, -1, 0];
-const positionArray = [1, 1, 0, 1, -1, 0, 0, 1, 0];
-const positionAttribute = new three.BufferAttribute(new Float32Array(positionArray), 3);
-const indexAttribute = new three.BufferAttribute(new Uint8Array(earcut(positionArray, null, 3)), 1);
-const geometry = new three.BufferGeometry();
-console.log(indexAttribute);
-geometry.setAttribute('position', positionAttribute);
-geometry.setIndex(indexAttribute);
-geometry.computeVertexNormals();
-geometry.computeBoundingSphere();
-geometry.computeBoundingBox();
-
+//
 const colors = new Uint8Array([0, 128]);
 const gradientMap = new three.DataTexture(colors, colors.length, 1, three.RedFormat);
 
 gradientMap.needsUpdate = true;
 
 const material = new three.MeshToonMaterial({ color: 0x8fa6c3, gradientMap, wireframe: false, side: three.DoubleSide });
-const mesh = new three.Mesh(geometry, material);
 
-scene.add(mesh);
+//
+const meshs = geometries.map(g => new three.Mesh(g, material));
+
+meshs.forEach((mesh, index) => {
+    //
+    mesh.scale.setY(0.4).setX(Math.random() * 3 + 1);
+    mesh.rotateY((directions[index] * Math.PI) / 2);
+    mesh.updateMatrix();
+
+    //
+    if (index === 0) return;
+
+    const prevMesh = meshs[index - 1];
+    const prevVector3 = new three.Vector3(0.5, 0.5, 0).applyMatrix4(prevMesh.matrix);
+    const nextVector3 = new three.Vector3(-0.5, 0.5, 0).applyMatrix4(mesh.matrix);
+
+    mesh.position.add(prevVector3.clone().sub(nextVector3));
+    mesh.updateMatrix();
+});
+
+const loading = new three.Group().add(...meshs);
+
+//
+const points = [];
+
+points.push(new three.Vector3(-0.5, 0.5, 0).applyMatrix4(meshs[0].matrix));
+meshs.forEach(mesh => points.push(new three.Vector3(0.5, 0.5, 0).applyMatrix4(mesh.matrix)));
+[...meshs].reverse().forEach(mesh => points.push(new three.Vector3(0.5, -0.5, 0).applyMatrix4(mesh.matrix)));
+points.push(new three.Vector3(-0.5, -0.5, 0).applyMatrix4(meshs[0].matrix));
+points.push(points[0]);
+
+const linePositions = [];
+
+for (const point of points) {
+    linePositions.push(...point.toArray());
+}
+
+const line = new three.Line(
+    new three.BufferGeometry().setFromPoints(points),
+    new three.MeshBasicMaterial({ color: 0xff0000 }),
+);
+
+const fatLineGeometry = new LineGeometry().setPositions(linePositions);
+const fatLineMaterial = new LineMaterial({
+    color: 0x000000,
+    linewidth: 0.05,
+    vertexColors: false,
+    dashed: false,
+    alphaToCoverage: false,
+});
+fatLineMaterial.worldUnits = true;
+fatLineMaterial.needsUpdate = true;
+
+const fatLine = new Line2(fatLineGeometry, fatLineMaterial);
